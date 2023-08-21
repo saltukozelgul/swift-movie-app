af2435d7f37b8cf8417fe2376876bc02466ca835
saltukozelgul <saltukozelgul@gmail.com>
2023-08-21 14:23:00 +0300

    added previews for readme

--- MovieProject/Networking/NetworkConstants.swift
+++ MovieProject/Networking/NetworkConstants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct NetworkConstants {
-    static let apiKey = "e17c0c151bd8b342a611a49b92e038bc"
+    static let apiKey = "i_have_regenareted_my_api_key_so_add_your_key"
     static let baseImageUrl = "https://image.tmdb.org/t/p/w500"
     static let baseImageUrlForOriginalSize = "https://image.tmdb.org/t/p/original"
     static let isoCode = NSLocalizedString("isoCode", comment: "iso code for api for localization")
